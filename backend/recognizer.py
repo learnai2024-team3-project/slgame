4877c22e2202dfc5a92da7ed89150384766bce6c
Sin-iu Ho <jb22621550@gmail.com>
2024-08-16 15:07:20 +0800

    Add recognize_view, tutorial_view; delete gust0811.ipynb

--- backend/recognizer.py
+++ backend/recognizer.py
@@ -2,37 +2,39 @@ import cv2
 import torch
 from ultralytics import YOLO
 
-# 检查并使用GPU或CPU
+# If a GPU (CUDA) is available, use it; 
+# otherwise, fall back to using the CPU.
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f'Using device: {device}')
 
-# 加载模型并将其移动到设备
+# Load the model and move it to the device
 model = YOLO('./models/best.pt')
 model.to(device)
 
+
 def recognize_image(image):
-    # 找到最高置信度的检测结果
+    # Find the detection result with the highest confidence
     best_result = None
     max_conf = 0
-    
-    # 设置置信度阈值
+
+    # Set the confidence threshold
     confidence_threshold = 0.25
-    
+
     results = model(image, conf=confidence_threshold)
 
     for result in results:
-        boxes = result.boxes.xyxy.cpu().numpy()  # 获取边界框
-        confs = result.boxes.conf.cpu().numpy()  # 获取置信度
-        labels = result.boxes.cls.cpu().numpy()  # 获取分类标签
-        
+        boxes = result.boxes.xyxy.cpu().numpy()  # Get bounding boxes
+        confs = result.boxes.conf.cpu().numpy()  # Get confidences
+        labels = result.boxes.cls.cpu().numpy()  # Get class labels
+
         for box, conf, label in zip(boxes, confs, labels):
             if conf > max_conf:
                 max_conf = conf
                 best_result = (box, conf, label)
 
-    # 绘制最高置信度的检测结果
+    # Draw the detection result with the highest confidence
     if best_result is not None:
         box, conf, label = best_result
         return (model.names[int(label)], conf)
-    
+
     return ("", 0.0)
