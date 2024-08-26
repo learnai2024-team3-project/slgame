5371a2fd30d7d6c5a0f322de84702f1959e0392d
Sin-iu Ho <jb22621550@gmail.com>
2024-08-26 02:29:12 +0000

    Add gdown, opencv-python, etc. to requirements.txt

--- models/download_model.sh
+++ models/download_model.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# 檢查 gdown 是否已安裝
-if ! pip show gdown > /dev/null 2>&1; then
-    echo "gdown is not installed. Installing gdown..."
-    pip install gdown
-else
-    echo "gdown is already installed."
-fi
-
-
 gdown 'https://docs.google.com/uc?id=10A6zKDeXzp65F4-oi4ZAD4bG9VxAojJy' -O ~/slgame/models/best.pt
 
 
